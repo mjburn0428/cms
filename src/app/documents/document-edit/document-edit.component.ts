86080cb0de65884f303eb7fa02da9ab025caab3b
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-11-09 17:33:33 -0700

    Updated to add documents

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm, FormsModule } from '@angular/forms'; // Import FormsModule here
+import { Document } from '../models/document.model';
 
 @Component({
   selector: 'cms-document-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Add FormsModule to imports array
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
 export class DocumentEditComponent {
+  originalDocument: Document | null = null;
+  document: Document = { id: 0, name: '', description: '', url: '' };
+  editMode: boolean = false;
 
+  constructor(private router: Router) {}
+
+  onCancel() {
+    this.router.navigate(['/documents']); // Adjust this route as necessary
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+    // Add form submission logic here
+  }
 }
