1ba3f0c6fc8c6478999ed4cff9709e7dc5e9627e
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-11-09 18:09:31 -0700

    Updated for edit form

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,28 +1,64 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgForm, FormsModule } from '@angular/forms'; // Import FormsModule here
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { NgForm, FormsModule } from '@angular/forms'; // Import FormsModule
 import { Document } from '../models/document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
   standalone: true,
-  imports: [FormsModule], // Add FormsModule to imports array
+  imports: [FormsModule], // Add FormsModule here
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
   originalDocument: Document | null = null;
   document: Document = { id: 0, name: '', description: '', url: '' };
   editMode: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  onCancel() {
-    this.router.navigate(['/documents']); // Adjust this route as necessary
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = +params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.documentService.getDocument(id) || null;
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument)); // Clone the document
+    });
   }
 
   onSubmit(form: NgForm) {
-    if (!form.valid) return;
-    // Add form submission logic here
+    const value = form.value;
+    const newDocument = new Document(
+      this.document.id,
+      value.name,
+      value.description,
+      value.url
+    );
+
+    if (this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    
+    this.router.navigate(['/documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
   }
 }
+
